63ce912dcc614373852d8034280c9df740aa8c57
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-02-07 23:19:21 -0800

    Added jquery to portfolio section.

--- js/script.js
+++ js/script.js
@@ -46,7 +46,7 @@ $(function() {
 
 
 /* ================================
-|   |   |   Progress Bars
+|   |   |   Responsive Tabs
 ================================ */
 
 $(function () {
@@ -55,4 +55,26 @@ $(function () {
         animation: 'slide'
     });
 
+});
+
+
+
+/* ================================
+|   |   |   Portfolio
+================================ */
+
+
+$('.gallery-list-item').click(function () {
+    let value = $(this).attr('data-filter');
+    if(value === 'all') {
+        $('.filter').show(300);
+    }else {
+        $('.filter').not('.' + value).hide(300);
+        $('.filter').filter('.' + value).show(300);
+    }
+});
+
+$('.gallery-list-item').click(function () {
+    $(this).addClass('active-item').siblings()
+        .removeClass('active-item');
 });
