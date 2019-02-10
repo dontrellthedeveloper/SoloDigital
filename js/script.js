af20d37d0f3ce5a06f4a305105d82329ebf94a09
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-02-09 19:20:22 -0800

    Added navigation section part 2

--- js/script.js
+++ js/script.js
@@ -162,9 +162,9 @@ $(window).on('load', function () {
 
     // 2. Add Marker
     var marker = new google.maps.Marker({
-       position: myLatlng,
-       map: map,
-       title: "Click To See Address"
+        position: myLatlng,
+        map: map,
+        title: "Click To See Address"
     });
 
     // 3. Add Info Window
@@ -177,7 +177,71 @@ $(window).on('load', function () {
         infowindow.open(map, marker);
     });
 
+
 });
+/* ================================
+|   |   |   Navigation
+================================ */
+
+/* Show & Hide White Navigation */
+$(function () {
+
+    // show/hide nav on page load
+    showHideNav();
+
+    $(window).scroll(function () {
+
+        // show/hide nav on window's scroll
+        showHideNav();
+
+    });
+    
+    function showHideNav() {
+
+        if( $(window).scrollTop() > 50 ) {
+
+            // Show white nav
+            $("nav").addClass("white-nav-top");
+
+            // Show dark logo
+            $(".navbar-brand img").attr("src", "img/logo/logo-dark.png");
+
+            // Show back to top
+            $("#back-to-top").fadeIn();
+
+        } else {
+
+            // Hide white nav
+            $("nav").removeClass("white-nav-top");
+
+            // Show logo
+            $(".navbar-brand img").attr("src", "img/logo/logo.png");
+
+            // Show back to top
+            $("#back-to-top").fadeOut();
+        }
+    }
+
+});
+
+// Smooth Scrolling
+$(function () {
+
+    $("a.smooth-scroll").click(function(event) {
+
+        event.preventDefault();
+
+        // get selection id like #about, #services, #work, #team and etc.
+        var section_id = $(this).attr("href");
+
+        $("html, body").animate({
+            scrollTop: $(section_id).offset().top - 64
+        }, 1250, "easeInOutExpo");
+
+    });
+
+});
+
 
 
 
