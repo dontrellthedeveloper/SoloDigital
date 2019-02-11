5effceb9080e851c0de7b69250a00cf98251654d
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-02-11 01:26:17 -0800

    Added Media Queries for Extra Small Devices (iPhone). Added menu button

--- js/script.js
+++ js/script.js
@@ -147,7 +147,18 @@ $(function() {
         autoplayHoverPause: true,
         nav: true,
         dots: false,
-        navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+        navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+        responsive: {
+            0: {
+                items: 2
+            },
+            480: {
+                items: 3
+            },
+            768: {
+                items: 6
+            }
+        }
     });
 });
 
@@ -183,6 +194,15 @@ $(window).on('load', function () {
     // Show info window when user clicks marker
     marker.addListener('click', function () {
         infowindow.open(map, marker);
+    });
+
+
+    // 4. Resize Function
+    google.maps.event.addDomListener(window, 'resize', function() {
+
+        var center = map.getCenter();
+        google.maps.event.trigger(map, 'resize');
+        map.setCenter(center);
     });
 
 
@@ -248,6 +268,23 @@ $(function () {
 
     });
 
+});
+
+
+
+/* ================================
+|   |   |   Mobile menu
+================================ */
+
+$(function() {
+
+    $("#mobile-nav-open-btn").click(function() {
+       $("#mobile-nav").css("height", "100%");
+    });
+
+    $("#mobile-nav-close-btn, #mobile-nav a").click(function() {
+        $("#mobile-nav").css("height", "0%");
+    });
 });
 
 
